96fa7ea7bdec594074d1466490aabd313c79471e
Quentin Pascal <62887758+QuentinP-PEIP@users.noreply.github.com>
2021-04-15 08:32:06 +0200

    Add files via upload

--- Liste commentaire user.py
+++ Liste commentaire user.py
@@ -1,53 +1,50 @@
-import requests
+import requests #requests is use to get an URL
 from bs4 import BeautifulSoup
 
 
-def liste_Com_User(username):
+def liste_Com_User(username): #we need the username of the user, the goal is to print a list of all his comments
     
 
     com =[]
     comF = []
     listeCom = []
     
-    reqUti = requests.get('https://www.tripadvisor.fr/Profile/'+username)
+    reqUti = requests.get('https://www.tripadvisor.fr/Profile/'+username) #we get the profile URL for an user
         
-    if reqUti.status_code == 404:
+    if reqUti.status_code == 404: #if the page doesn't exist
         
         print('Utilisateur introuvable')
         return 0
         
     soupUti = BeautifulSoup(reqUti.text, "lxml")
     
-    for a in soupUti.find(id='content')("a"):
+    for a in soupUti.find(id='content')("a"): #we search in the div "content" every link for the reviews
             
         urlCom = a.get('href')
         if 'ShowUserReviews' in str(urlCom):
-            com.append(urlCom)
+            com.append(urlCom) #we add them in a list
             
-    comF = []
     unSurDeux = True
     
-    for i in com:
+    for i in com: #every links are present two times in the list
         
         if unSurDeux:
             
-            comF.append(i)
+            comF.append(i) #we add to a new list, one in two of the URL
         
         unSurDeux = not unSurDeux 
-        
-    #print(comF)
             
     
     for i in range(len(comF)):
         
-        reqCom = requests.get('https://www.tripadvisor.fr/'+comF[i])
+        reqCom = requests.get('https://www.tripadvisor.fr/'+comF[i]) #we reach these URLs
         soupCom = BeautifulSoup(reqCom.text, "lxml")
     
         #Contenu des commentaires
     
         for p in soupCom.find("div", {"class": "entry"})('p'):
         
-            listeCom.append(p.text)
+            listeCom.append(p.text) #add the comment's text in a list
             
     return listeCom
 
