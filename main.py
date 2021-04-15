9653cae149e860bb241fa710216e6b233b2753c9
Forray_Gabriel <gabriel.forray@gmail.com>
2021-04-15 08:41:28 +0200

    Cleaning

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import trip_asvisor as ta 
+import Liste_commentaire_user as liste_com
+
+
 
 url = input("Url du restaurant : \n")
 
@@ -11,9 +14,16 @@ infos = ta.getUsernames(url)
 for i in infos:
 	try :
 		f.write("L'utilisateur " + i[0]+ " a écrit la review :\n" + str(i[1]) + "\n\n")
-		f.write("Ainsi que les reviews suivantes pour d'autres établissements :\n")
+		other_reviews = liste_com.liste_Com_User(i[0])
+		print(i[0])
+		if other_reviews:
+			f.write("Ainsi que les reviews suivantes pour d'autres établissements :\n")
+			for rev in other_reviews:
+				f.write(rev+"\n\n")
+
 		f.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n")
 	except :
 		f.write("L'utilisateur a saisie un caractère interdit")
 
 f.close()
+
